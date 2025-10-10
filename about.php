5aabe95c942da8ecbedaa5ae03ce7f1787e0a065
John Kelley <jbkmidnight@gmail.com>
2025-10-10 03:19:36 +0000

    added more stuff for week 4 homework

--- about.php
+++ about.php
@@ -23,6 +23,26 @@
                     <p>Hello!! On this page I am going to tell you a little about myself.</p>
                     <p>My name is John Benjamin Kelley and i live in virginia. I just turned 18 years old on the 19th of september. some of the things i like to do is fish, hunt deer, dove and squirrelS. Another thing i like to do is build stuff like Legos and model cars. I like to liston to music while i do pretty much everything. some of my favorite artists are NF, Forrest Frank, and Tobby Mac.</p>
                     <p>My favorite song right now is Riptide by Vance Joy. I am currently in my last year of highschool. After i finish highschool i plan on going to Wytheville community College for their two year Electricians program but God may change my plans like He has before and i've just got to trust Him. He's got a plan for my life He's got someone out there for me He's got the right job for me. Jeremiah 29:11 says "For I know the plans I have for you,"declarers the Lord,"Plans to prosper you and not harm you,plans to give you hope and a future.".</p>
+                    <table>
+                <thead>
+                        <tr>
+                                <th>Hobby</th>
+                                <th>About</th>
+                        </tr>
+                    </thead>      
+                    <tbody>
+                        </tr> 
+                                <td>Deer Hunting</td>
+                                <td>Being in the woods and in the silence of it all is just always fun. Until the gun goes off then its just like did I hit it or did I miss. Some days its cold and rainy but other times its dry and not cold. sitting up in a tree for bow season can be either really fun or really boring. </td>
+                        </tr>
+                        <tr>
+                                <td>Basketball</td>
+                                <td>Basketball is a fun and fastpast sport that takes team work. You have to learn plays to run in game and if not everyone knows them then they wont work. Everyone is a coach in a way. If someone doesn't know a play and you do try and teach them it.</td>
+                        <tr>
+                        </tr>
+                                <td>Building Legos while listening to music</td>
+                                <td>Building legos takes some time but once you get into building one you dont want to stop. listening to music while doing it kinda puts you into your own little world. Legos are really expensive ]:.</td>
+                </tbody>
                     <div class="about__socials"><?php
 
                         // Set socials.
--- html.php
+++ html.php
@@ -9,7 +9,46 @@
         <h1 id="main-heading">9/26/25</h1>
         <P> my name is Inigo Montoya</P>
         <div Class="main">
-            <p Style="color:red"> you killed my father. prepare to die</p>
+            <a  href="https://youTube.com"><p Style="color:red"> you killed my father. prepare to die</p>
+            <a  href="https://youTube.com" target="_blank">youTube</a>
+            <table>
+                <thead>
+                        <tr>
+                                <th>Hobby</th>
+                                <th>Name</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    </tr> 
+                                <td>Deer Hunting</td>
+                                <td>John</td>
+                        </tr>       
+                        <tr>
+                                <td>Basketball</td>
+                                <td>William</td>
+                        <tr>
+                </tbody>
+            </table></php
+            print_r(value: _post)
+            echo '<pre>'; ?>
+
+            <form method="post"
+                </p>
+                     <label for="email">Email:</label>
+                     <input type="email" name="email" placeholder="Enter your email..."/>
+                </p>
+                <p>
+                    <label for="Gender">Gender</label>
+                    <select>
+                        <option value="">--Please choose an option--</option>
+                        <option value="Male">Male</option>
+                        <option value="Female">Female</option>
+                    <select>
+                </p>
+                <p>
+                        <input type="submit" value="send"
+                </p>
+            </form>
         </div>
     </body>
 </html>
